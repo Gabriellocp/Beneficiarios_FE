4f05002cb46d4941f8e5b37b3d71cb224f201079
gabriellocp <gabriel_campista@hotmail.com>
2024-02-27 14:03:11 -0300

    chore: Add path alias
    In webpack.config, resolve alias was added as '@' for './src' folder and also, added jsconfig.json file to enable intellisense

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,39 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
-    output:{
+    output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'build[fullhash].js',
         publicPath: '/',
         clean: true
     },
-    module:{
+    module: {
         rules: [
             {
-                test:/\.js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             },
-            {  
+            {
                 test: /\.[s]css$/,
                 exclude: /node_modules/,
                 use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader', options:{modules: true}},
-                    {loader: 'sass-loader'},
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: { modules: true } },
+                    { loader: 'sass-loader' }
                 ]
-            }   
+            }
         ]
     },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
     plugins: [
-        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
     ],
-    devServer:{
-        port: 3002,
+    devServer: {
+        port: 3002
     }
 }
