91018372320d1efe1ca005e36f3129a1d90ba373
gabriellocp <gabriel_campista@hotmail.com>
2024-02-27 13:23:55 -0300

    chore: Initial commit with configuration
    Configure basic dependencies to start React project
    Configure webpack and webpackDevServer
    Configure Babel and Sass/css/style loaders

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output:{
+        path: path.resolve(__dirname, 'build'),
+        filename: 'build[fullhash].js',
+        publicPath: '/',
+        clean: true
+    },
+    module:{
+        rules: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {  
+                test: /\.[s]css$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader', options:{modules: true}},
+                    {loader: 'sass-loader'},
+                ]
+            }   
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})
+    ],
+    devServer:{
+        port: 3002,
+    }
+}
