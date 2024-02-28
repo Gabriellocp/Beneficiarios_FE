5f9e37be2f42c4ad225fa76c743963389f3674d4
gabriellocp <gabriel_campista@hotmail.com>
2024-02-28 16:31:20 -0300

    chore: Add webpack rule to load images

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
                     { loader: 'css-loader', options: { modules: true } },
                     { loader: 'sass-loader' }
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
     },
