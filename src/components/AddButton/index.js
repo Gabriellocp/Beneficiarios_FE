d7a0ae9f300a8ed2c82349e16a7cde8f724d7fa2
gabriellocp <gabriel_campista@hotmail.com>
2024-02-29 14:54:04 -0300

    feat: Add AddButton component

---
+++ src/components/AddButton/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import add from '@/assets/icons/add.svg'
+import styles from './styles.scss'
+import PropTypes from 'prop-types'
+import Button from '../Button'
+export default function AddButton({ label, onClick }) {
+    return (
+        <Button onClick={onClick} type='secondary'>
+            <img width={20} src={add} alt="adicionar" />
+            <span className={styles.label}>{label}</span>
+        </Button>
+
+    )
+}
+
+AddButton.propTypes = {
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.scss'
-import Header from './components/Header'
+import AddButton from './components/AddButton'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
-    <Header />
+    <AddButton label={'Novo beneficiário'} />
 )
