4eed80db242190d1a1ca857730f11422f7f52eae
gabriellocp <gabriel_campista@hotmail.com>
2024-02-29 16:25:11 -0300

    refactor: Style changes in BeneficiaryCard and Input

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.scss'
-import MemberCard from './components/MemberCard'
+import App from './components/App'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
-    <MemberCard />
+    <App />
 )
